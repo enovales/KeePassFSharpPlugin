3e343bb56c0880c3f1001970ba8619bc87abfed2
Erik Novales <enovales@codehitmen.com>
2019-08-19 23:00:30 -0700

    initial commit

---
+++ KeePassFSharpPlugin/KeePassFSharpPluginExt.fs
@@ -0,0 +1,31 @@
+﻿namespace KeePassFSharpTest
+
+open KeePass.Plugins
+open System
+open System.Windows.Forms
+
+type KeePassFSharpPluginExt() = 
+    inherit Plugin()
+    let mutable private m_host = null
+    override this.Initialize(host: IPluginHost): bool = 
+        if (host = null) then
+            false
+        else
+            m_host <- host
+            true
+        
+    override this.Terminate() = 
+        ()
+    
+    override this.GetMenuItem(t: PluginMenuType): ToolStripMenuItem = 
+        match t with
+        | PluginMenuType.Main -> 
+            let tsmi = new ToolStripMenuItem()
+            tsmi.Text <- "Test F# Plugin Options"
+            tsmi.Click.AddHandler(new EventHandler(this.OnOptionsClicked))
+            tsmi
+        | _ -> null
+
+    member private this.OnOptionsClicked(sender: obj)(e: EventArgs) = 
+        MessageBox.Show("You clicked the menu item for this plugin!") |> ignore
+        ()
